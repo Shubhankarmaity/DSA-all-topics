edb78e7a901cbe807e919aeec5bcf901405f6088
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-04-06 22:41:01 +0530

    updated DP

--- fibonaciDP.java
+++ fibonaciDP.java
@@ -9,6 +9,7 @@ public class fibonaciDP {
         f[n]=fib(n-1, f)+fib(n-2, f);
         return f[n];
     }
+    //tabulation process
     public static int fibTabulation(int n){
         int dp[]=new int[n+1];
         dp[0]=0;
